09f9310f26d9d682e9f4b68d7e1f6fbde84ed903
koin <kangyueluo777@gmail.com>
2019-08-09 13:29:26 +0800

    Initial commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'MyButton.dart';
+
+class MyHomePage extends StatelessWidget{
+  final String title;
+
+  MyHomePage({Key key,this.title}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text(title),
+      ),
+      body: Center(child: new MyButton()),
+    );
+  }
+}
